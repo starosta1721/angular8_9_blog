1d5e7bb6f9072a967d32af47a58968c04610deab
starosta1721 <starosta1721@gmail.com>
2020-07-08 10:37:40 +0300

    Create login page with simple validators for email and password

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,6 +6,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -18,6 +19,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forChild( [
             {
                 path: '', component: AdminLayoutComponent, children: [
